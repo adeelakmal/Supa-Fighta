7cffbec18fb8fd10ba9e3b2d0dcb4cb44aa09368
adeelakmal <adeelakmal0@gmail.com>
2025-05-08 01:31:17 +0500

    basic database setup

--- supa-fighta/src/wsServer.js
+++ supa-fighta/src/wsServer.js
@@ -1,13 +1,16 @@
 // Initialize a server here for websockets 
 const WebSocket = require('ws');
+const pool = require('./config/db')
 const { HandleMessage, HandleClose } = require('./controllers/lobbyController');
 
 
 const setupWebSocketServer = (port) => {
 const wss = new WebSocket.Server({ port: port });
-  wss.on('connection', (ws) => {
+  wss.on('connection', async (ws) => {
     ws.id = crypto.randomUUID();
     console.log(`A new player - ${ws.id} has joined the lobby`);
+    let result = await pool.query(`INSERT INTO public."Players" (id) VALUES ($1) RETURNING *`, [ws.id])
+    console.log("Player inserted into db:", result)
     
     ws.on('message', message => HandleMessage(ws, message))
     ws.on('close', () => HandleClose(ws));

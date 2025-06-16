48948762817430f2377ded711206404391804ad3
adeelakmal <adeelakmal0@gmail.com>
2025-06-16 22:37:40 +0500

    added python game client

---
+++ supa-fighta-client/src/game/player.py
@@ -0,0 +1,36 @@
+import pygame
+import config
+
+class Player:
+    def __init__(self, x, y, net):
+        self.rect = pygame.Rect(x, y, config.PLAYER_SIZE, config.PLAYER_SIZE)
+        self.color = config.PLAYER_COLOR
+        self.speed = 5
+        self.net   = net                 # websocket client injected
+
+    def handle_keys(self):
+        keys = pygame.key.get_pressed()
+
+        moved = False
+        dx = dy = 0
+
+        if keys[pygame.K_LEFT]:
+            dx -= self.speed
+            moved = True
+        if keys[pygame.K_RIGHT]:
+            dx += self.speed
+            moved = True
+        if keys[pygame.K_UP]:
+            dy -= self.speed
+            moved = True
+        if keys[pygame.K_DOWN]:
+            dy += self.speed
+            moved = True
+
+        if moved:
+            self.rect.move_ip(dx, dy)    # local prediction
+            # tell the server; keep the payload tiny
+            self.net.send("move", {"dx": dx, "dy": dy})
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)

42626feb3fa42719c96c00af6a48e4abe8fb17d0
adeelakmal <adeelakmal0@gmail.com>
2025-06-18 01:00:02 +0500

    fix bug lobby error

--- supa-fighta-client/src/game/player.py
+++ supa-fighta-client/src/game/player.py
@@ -6,31 +6,29 @@ class Player:
         self.rect = pygame.Rect(x, y, config.PLAYER_SIZE, config.PLAYER_SIZE)
         self.color = config.PLAYER_COLOR
         self.speed = 5
-        self.net   = net                 # websocket client injected
-
+        self.net = net
     def handle_keys(self):
         keys = pygame.key.get_pressed()
 
         moved = False
-        dx = dy = 0
-
+        
         if keys[pygame.K_LEFT]:
-            dx -= self.speed
+            self.rect.x -= self.speed
             moved = True
         if keys[pygame.K_RIGHT]:
-            dx += self.speed
+            self.rect.x += self.speed
             moved = True
         if keys[pygame.K_UP]:
-            dy -= self.speed
+            self.rect.y -= self.speed
             moved = True
         if keys[pygame.K_DOWN]:
-            dy += self.speed
+            self.rect.y += self.speed
             moved = True
 
-        if moved:
-            self.rect.move_ip(dx, dy)    # local prediction
-            # tell the server; keep the payload tiny
-            self.net.send("move", {"dx": dx, "dy": dy})
+        # if moved:
+        #     self.rect.move_ip(dx, dy)    # local prediction
+        #     # tell the server; keep the payload tiny
+        #     self.net.send("move", {"dx": dx, "dy": dy})
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)

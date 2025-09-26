225ff74275689e622753d65d943e356abd51b08f
Hamza Tariq <129800852+pakoramaster@users.noreply.github.com>
2025-09-26 19:23:25 -0400

    Updated the Main Menu UI OwO

--- supa-fighta-client/src/game/states/mainMenu.py
+++ supa-fighta-client/src/game/states/mainMenu.py
@@ -9,12 +9,12 @@ class MainMenuState:
         self.state_manager = state_manager
         self.title_font = pygame.font.Font(None,74)        
         self.buttons = [
-            Button(config.WINDOW_WIDTH//2 - 75,config.WINDOW_HEIGHT//2 - 50,"Start Game"),
-            Button(config.WINDOW_WIDTH//2 - 75,config.WINDOW_HEIGHT//2,"Settings"),
-            Button(config.WINDOW_WIDTH//2 - 75,config.WINDOW_HEIGHT//2 + 50,"Exit")
+            Button(22,config.WINDOW_HEIGHT - 190,"Join Lobby"),
+            Button(22,config.WINDOW_HEIGHT - 150,"Settings"),
+            Button(25,config.WINDOW_HEIGHT - 110,"Exit")
         ]
         self.background_sprites = SpriteSheet("./supa-fighta-client/assets/background.png").get_sprites(config.WINDOW_WIDTH, config.WINDOW_HEIGHT, 1, 8)
-        self.background = Animator(self.background_sprites, 10)
+        self.background = Animator(self.background_sprites, 6)
     def enter(self):
         self.running = True
     def exit(self):
@@ -23,15 +23,12 @@ class MainMenuState:
         self.background.update()
     def draw(self, screen: pygame.Surface):
         self.background.draw(screen)
-        title_surface = self.title_font.render("Supa Fighta", True, (0, 0, 0))
-        rect = title_surface.get_rect(center=(config.WINDOW_WIDTH //2 , 50)) # Center the title
-        screen.blit(title_surface, rect)
         for button in self.buttons:
             button.draw(screen)
     def handle_event(self, event):
         for button in self.buttons:
             result = button.handle_event(event)
-            if result == "Start Game":
+            if result == "Join Lobby":
                 self.state_manager.change_state("lobby")
             elif result == "Settings":
                 self.state_manager.change_state("settings")
